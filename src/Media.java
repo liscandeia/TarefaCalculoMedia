d593ed3fcb9f024425fe9fcc7ae11888946200b1
liscandeia <liscandeia523@gmail.com>
2023-10-21 21:44:52 -0300

    Subindo Tarefa do Módulo 8

---
+++ src/Media.java
@@ -0,0 +1,15 @@
+public class Media {
+
+    public static void main (String args[]){
+        resultadoMedia();
+    }
+
+    public static void resultadoMedia(){
+        float nota1 = 3;
+        float nota2 = 9;
+        float nota3 = 10;
+        float nota4 = 8;
+        float media = (nota1 + nota2 + nota3 + nota4)/4;
+        System.out.println(media);
+    }
+}
